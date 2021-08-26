23c25f6cccf85035d2e5d0b6ff1172b78fb94520
JCamyre <JWcamry03@gmail.com>
2021-08-25 18:00:18 -0700

    Add routes and pages for Home and about page

--- portfoliosite/frontend/src/App.js
+++ portfoliosite/frontend/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+
 // Have React handle all url patterns for frontend, don't need urls.py
 function App() {
   return (
-    <div>
-      <p>Test1</p>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route path='/about' component={About} />
+      </Switch>
+    </Router>
  );
 }
 
