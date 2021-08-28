107091f68c20ac1ef07c499b794d0f6cc3f4ca6c
JCamyre <JWcamry03@gmail.com>
2021-08-27 19:18:39 -0700

    Add CSS for Navbar and App

--- frontend/src/components/Button1/Button1.jsx
+++ frontend/src/components/Button1/Button1.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './Button1.css';
 
+
 export default function Button1(props){
     // Allow button to change size?
     return(
-        <button class='b-secondary w-button' style={{}}>
-            {props.children}
-        </button>
+        <div>
+            <a href={props.link ? props.link : '#' } class='b-secondary w-button' style={{}}>
+                {props.children}
+            </a>
+        </div>
     )
 }
