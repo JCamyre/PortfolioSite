2fc70da24785d572480e70662d85cf450c1f7f3d
JCamyre <JWcamry03@gmail.com>
2021-08-27 18:12:06 -0700

    Convert portfoliosite to pure React

---
+++ frontend/src/components/ColorfulButton.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './ColorfulButton.css';
+
+// difference from export default function Button
+export default function Button(props){
+    return (
+        <button class='button-primary'>
+            Button
+        </button>
+    )
+}
